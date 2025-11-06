eb068db244488e5777a6e625fabdccfef5a3d88c
ProyectoZuloESP <proyecto.zulo.esp@gmail.com>
2025-11-06 20:17:18 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,21 @@
+  <!-- Footer -->
+  <footer class="site-footer">
+    <div class="container">
+      <p class="footer-slogan">
+        Roomix — Conectando personas, creando hogares.
+      </p>
+      <p class="footer-legal">
+        <a href="terminos.php">Términos y Condiciones</a> |
+        <a href="privacidad.php">Política de Privacidad</a> |
+        <a href="cookies.php">Política de Cookies</a> <br><br>
+        <span id="year"></span>© Todos los derechos reservados.
+      </p>
+    </div>
+  </footer>
+
+  <!-- Script común -->
+  <script>
+    document.getElementById('year').textContent = new Date().getFullYear()
+  </script>
+</body>
+</html>
